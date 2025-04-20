120560885a20707ba687c7d00bf1cea327815be1
exmanek <szymon.podlasiak.111@gmail.com>
2025-04-20 11:31:07 +0200

    fix menu

--- js/script.js
+++ js/script.js
@@ -1,7 +1,15 @@
 const navBtn = document.querySelector("#nav-burgericon")
 const nav = document.querySelector(".nav")
+const allNavItems = document.querySelectorAll(".nav__item")
 
 
 navBtn.addEventListener("click", function() {
     nav.classList.toggle("active")
+    allNavItems.forEach((item) => {
+        item.addEventListener("click", function(){
+            nav.classList.remove("active")
+        })
+    })
+
+
 })
